d8083b58dea25986b7f7edda21628b0f9e9693a2
ddhung <ddhung@ambition.vn>
2020-09-17 09:18:16 +0700

    Crawl data and display it

--- DataFlair_NewsAggregator/news/models.py
+++ DataFlair_NewsAggregator/news/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Headline(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.URLField(null=True, blank=True)
+    original_price = models.CharField(null=True, max_length=50)
+    promotional_price = models.CharField(null=True, max_length=50)
+    url = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_original_price(self):
+        if self.original_price != None:
+            return self.original_price
+        return ""
+
+    def get_promotional_price(self):
+        if self.promotional_price != None:
+            return self.promotional_price
+        return ""
--- DataFlair_NewsAggregator/news/views.py
+++ DataFlair_NewsAggregator/news/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+import time
+from django.shortcuts import render, redirect
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+from news.models import Headline
 
-# Create your views here.
+def scrape(request):
+    url = "https://www.thegioididong.com/dtdd"
+    driver = webdriver.Chrome(executable_path=r"C:/Users/Ambition 61/Downloads/chromedriver.exe")
+    driver.get(url)
+    time.sleep(3)
+    while len(driver.find_elements_by_css_selector(".viewmore")) != 0:
+        try:
+            driver.find_elements_by_css_selector(".viewmore")[0].click()
+        except:
+            pass
+        time.sleep(1)
+    time.sleep(1)
+    phones = driver.find_elements_by_css_selector(".item")
+    for phone in phones:
+        main = phone.find_elements_by_css_selector("a")[0]
+        link = main.get_attribute("href")
+        image = main.find_elements_by_css_selector("img")[0]
+        if image.get_attribute("data-original") != None:
+            image_src = str(image.get_attribute("data-original"))
+        else:
+            image_src = str(image.get_attribute("src"))
+        title = main.find_elements_by_css_selector("h3")[0]
+        price = main.find_elements_by_css_selector(".price")[0]
+        original_price = price.find_elements_by_css_selector("span")
+        promotional_price = price.find_elements_by_css_selector("strong")
+        original_price = original_price[0].text if len(original_price) != 0 else None
+        promotional_price = promotional_price[0].text if len(promotional_price) != 0 else None
+        new_headline = Headline()
+        new_headline.title = title.text
+        new_headline.url = link
+        new_headline.image = image_src
+        new_headline.original_price = original_price
+        new_headline.promotional_price = promotional_price
+        new_headline.save()
+    return redirect("../")
+
+def news_list(request):
+    headlines = Headline.objects.all()
+    context = {
+        "object_list": headlines,
+    }
+    return render(request, "news/home.html", context)
